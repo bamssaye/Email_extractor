fa01409db512cddb61ab609f00342b011bf80a6d
AMSSAYEF BRAHIM <AMSSAYEF.BRAHIM@ofppt-edu.ma>
2023-09-16 20:21:30 +0100

    Email Extractor

---
+++ README.md
@@ -0,0 +1,32 @@
+# Email Address Extractor Script
+
+This Bash script is designed to process text files in a directory (`input_file_txt`) and extract email addresses. It provides an option to filter email addresses based on a specified domain.
+
+## Usage
+
+1. **Clone the Repository**
+
+    ```bash
+    git clone https://github.com/username/repository.git
+    ```
+
+2. **Navigate to the Directory**
+
+    ```bash
+    cd repository
+    ```
+
+3. **Run the Script**
+
+    ```bash
+    ./script.sh
+    ```
+
+4. **Follow the Prompts**
+
+   - Enter the domain you want to filter (e.g., `yandex.com`) or use `*` for all domains.
+   - Provide a name for the results file (e.g., `results_1.txt`).
+
+5. **Results**
+
+   The filtered email addresses will be saved in the specified results file.
---
+++ email_extract.sh
@@ -0,0 +1,41 @@
+#########################################
+##                                     ##
+##   BBBB   BBBBBB  BB  BB  BBBBBB     ##
+##   BB  B    BB    BB  BB    BB       ##
+##   BBBB     BB    BBBBBB    BB       ##
+##   BB  B    BB    BB  BB    BB       ##
+##   BBBB   BBBBBB  BB  BB  BBBBBB.    ##
+##                                     ##
+#########################################
+
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+FILES="$SCRIPT_DIR/input_file_txt/*"
+
+read -p "Enter Domain you want to filter (EX : yandex.com Or * for all domain): " DOMAIN_FILTER
+
+read -p "Enter Results file Name: (EX : results_1.txt) :  " RESULT_FILE
+
+RESULT_FILE="$SCRIPT_DIR/$RESULT_FILE"
+
+echo "Filter: $DOMAIN_FILTER" > "$RESULT_FILE"
+
+for f in $FILES
+do
+  name=$(basename "$f" ".txt")
+  echo "Processing $f file... [$name]"
+
+  if [ "$DOMAIN_FILTER" == "*" ]; then
+    perl -wne'while(/[\w\.\-]+@[\w\.\-]+\w+/g){print "$&\n"}' "$f" | sort -u > "$name.out"
+  else
+    perl -wne'while(/[\w\.\-]+@[\w\.\-]+\w+/g){print "$&\n"}' "$f" | grep "$DOMAIN_FILTER" | sort -u > "$name.out"
+  fi
+
+  cat "$name.out" >> "$RESULT_FILE" 
+done
+
+rm -rf *.out
+
+echo "Processing complete."
